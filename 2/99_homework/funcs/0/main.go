7491b345c394727db344550d14b318d2ad7e08e8
zxxz <viacheslav.modestov@gmail.com>
2018-02-20 00:29:40 +0300

    Sqrt Newton pass tests in dir funcs

--- 2/99_homework/funcs/0/main.go
+++ 2/99_homework/funcs/0/main.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 )
 
+// https://ru.wikipedia.org/wiki/Алгоритм_нахождения_корня_n-ной_степени)
 // TODO: Реализовать вычисление Квадратного корня
 func Sqrt(x float64) float64 {
-	return x
+	var a, a_k float64
+	f := func() (flt float64) {
+		flt = (0.5 * (a + (x / a)))
+		return
+	}
+
+	a = x / 2
+	a_k = f()
+    for i := 0; i < 7; i++ {
+		a = a_k
+		a_k = f()
+	}
+	return a_k
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(25))
 }
