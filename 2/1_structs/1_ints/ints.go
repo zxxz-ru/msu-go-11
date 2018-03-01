35e2df9447110312552e83d88d0468466df10477
zxxz <viacheslav.modestov@gmail.com>
2018-03-01 18:43:47 +0300

    fix 2/1_struct/1_ints

--- 2/1_structs/1_ints/ints.go
+++ 2/1_structs/1_ints/ints.go
@@ -10,6 +10,14 @@ type MyStruct struct {
 	Name string
 }
 
+func NewMyStruct() func(string) MyStruct {
+    id := 0
+return func(name string)MyStruct{
+    id = id + 1
+return MyStruct{id, name}
+}
+}
+
 type MyInt int
 
 type withFiles bool
@@ -23,19 +31,32 @@ func (m *MyInt) add(i MyInt) {
 }
 
 type mySliceStruct []MyStruct
-
-func (m mySliceStruct) Less(i int) bool
-func (m mySliceStruct) Len() int
-func (m mySliceStruct) Swap(i, j int)
+func (m mySliceStruct) Less(i, j int) bool{
+return m[i].Name < m[j].Name
+}
+func (m mySliceStruct) Len() int{
+return len(m)
+}
+func (m mySliceStruct) Swap(i, j int){
+m[i], m[j] = m[j], m[i]
+}
+func (m mySliceStruct) Sort() {
+sort.Sort(m)
+}
 
 func main() {
 	i := MyInt(0)
 
 	i.add(3)
 	i.showYourSelf()
+    var slc mySliceStruct
+    slc = make([]MyStruct, 0, 3)
+    constructor := NewMyStruct()
+slc = append(slc, constructor("Вася"))
+slc = append(slc, constructor("Слава"))
+slc = append(slc, constructor("Витя"))
+fmt.Println(slc)
+slc.Sort()
+fmt.Println(slc)
 }
 
-func sorter(sl []MyStruct) []MyStruct {
-	sort.Slice(sl, func(i, j int) {})
-	return sl
-}
